222510a3ed87610921e8944a4fddab143c11c8fe
Sora Lee <anysky23@icloud.com>
2024-08-01 07:01:05 +0900

    ros2 class

--- KODTA/topic_final/src/tpub.cpp
+++ KODTA/topic_final/src/tpub.cpp
@@ -1,15 +1,43 @@
-#include 
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/header.hpp"
+#include <chrono>
+#include <iostream>
 
+using namespace std;
+using namespace std::chrono_literals;
 
-
-class 
+class TimePub : public rclcpp::Node
 {
 public:
-    MessagePub
+    TimePub()
         : Node("tpub"), _i(0)
     {
         auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
-        _pub = this->create_publisher<std_msgs::msg::Header>("tpub", qos_profile);
+        _pub = this->create_publisher<std_msgs::msg::Header>("time", qos_profile);
+        _timer = this->create_wall_timer(1s, std::bind(&TimePub::publish_time_msg, this));
+        _clock = rclcpp::Clock(RCL_ROS_TIME);
     }
-    
+
+private:
+    int _i;
+    rclcpp::Publisher<std_msgs::msg::Header>::SharedPtr _pub;
+    rclcpp::TimerBase::SharedPtr _timer;
+    rclcpp::Clock _clock;
+    void publish_time_msg()
+    {
+        auto msg = std_msgs::msg::Header();
+        msg.stamp = _clock.now();
+        msg.frame_id = "tpub" + to_string(_i);
+        _pub->publish(msg);
+        _i++;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<TimePub>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 #####31Jul#### <br>
 message <br>
  - message interface = message form : <br>
-    1)topic(name,type) /pkg.cml, cmakelist -> pub-sub -> compile <br>
+    1)topic(name,type): +pkgxml, +cmakelist -> pub-sub -> compile <br>
     2)service(name,type) <br>
     3)action(name,type) <br>
 
