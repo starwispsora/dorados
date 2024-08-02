6b6b2a9ec331dfde1eef2e4a66002860c9a9555e
Sora Lee <anysky23@icloud.com>
2024-08-02 11:00:13 +0900

    ros2 class

--- KODTA/move_t_sim/src/moveTurtleBotcpp2.cpp
+++ KODTA/move_t_sim/src/moveTurtleBotcpp2.cpp
@@ -16,11 +16,23 @@ void MoveTurtleBot::publish_turtlesim_msg()
     switch (_i)
     {
     case 0:
-        if (_odom_msg.pose.pose.position.x < 0.3)
+        if (_odom_msg.pose.pose.position.x < 0.3 && _theta > -0.1 && _theta < 0.1)
         {
             // 직진
             msg.linear.x = 0.1;
             msg.angular.z = 0.0;
+        }
+        else if (_odom_msg.pose.pose.position.x < 0.3 && _theta < -0.1)
+        {
+            // 회전
+            msg.linear.x = 0.0;
+            msg.angular.z = 0.3;
+        }
+        else if (_odom_msg.pose.pose.position.x < 0.3 && _theta > 0.1)
+        {
+            // 회전
+            msg.linear.x = 0.0;
+            msg.angular.z = -0.3;
         }
         else if (_theta < 1.57)
         {
@@ -34,11 +46,23 @@ void MoveTurtleBot::publish_turtlesim_msg()
         }
         break;
     case 1:
-        if (_odom_msg.pose.pose.position.y < 0.3)
+        if (_odom_msg.pose.pose.position.y < 0.3 && _theta > 1.47 && _theta < 1.67)
         {
             // 직진
             msg.linear.x = 0.1;
             msg.angular.z = 0.0;
+        }
+        else if (_odom_msg.pose.pose.position.y < 0.3 && _theta < 1.47)
+        {
+            // 회전
+            msg.linear.x = 0.0;
+            msg.angular.z = 0.3;
+        }
+        else if (_odom_msg.pose.pose.position.y < 0.3 && _theta > 1.67)
+        {
+            // 회전
+            msg.linear.x = 0.0;
+            msg.angular.z = -0.3;
         }
         else if (_theta < 3.14 && _theta > 0)
         {
@@ -52,11 +76,29 @@ void MoveTurtleBot::publish_turtlesim_msg()
         }
         break;
     case 2:
-        if (_odom_msg.pose.pose.position.x > 0)
+        if (_odom_msg.pose.pose.position.x > 0 && _theta > 3.04)
         {
             // 직진
             msg.linear.x = 0.1;
             msg.angular.z = 0.0;
+        }
+        else if (_odom_msg.pose.pose.position.x > 0 && _theta < -3.04)
+        {
+            // 직진
+            msg.linear.x = 0.1;
+            msg.angular.z = 0.0;
+        }
+        else if (_odom_msg.pose.pose.position.x > 0 && _theta < 3.04)
+        {
+            // 회전
+            msg.linear.x = 0.0;
+            msg.angular.z = 0.3;
+        }
+        else if (_odom_msg.pose.pose.position.x > 0 && _theta > -3.04)
+        {
+            // 회전
+            msg.linear.x = 0.0;
+            msg.angular.z = -0.3;
         }
         else if (_theta < -1.57)
         {
@@ -70,11 +112,23 @@ void MoveTurtleBot::publish_turtlesim_msg()
         }
         break;
     case 3:
-        if (_odom_msg.pose.pose.position.y > 0)
+        if (_odom_msg.pose.pose.position.y > 0 && _theta < -1.47 && _theta > -1.67)
         {
             // 직진
             msg.linear.x = 0.1;
             msg.angular.z = 0.0;
+        }
+        else if (_odom_msg.pose.pose.position.y > 0 && _theta < -1.67)
+        {
+            // 회전
+            msg.linear.x = 0.0;
+            msg.angular.z = 0.3;
+        }
+        else if (_odom_msg.pose.pose.position.y > 0 && _theta > -1.47)
+        {
+            // 회전
+            msg.linear.x = 0.0;
+            msg.angular.z = -0.3;
         }
         else if (_theta < 0)
         {
@@ -95,6 +149,10 @@ void MoveTurtleBot::sub_odom_msg(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
     _odom_msg = *msg;
     tf2::Quaternion q(_odom_msg.pose.pose.orientation.x, _odom_msg.pose.pose.orientation.y, _odom_msg.pose.pose.orientation.z, _odom_msg.pose.pose.orientation.w);
-    _theta = q.getAngle() * 2 - 3.141592;
+
+    // _theta = q.getAngle() * 2 - 3.141592;
+    double _roll, _pitch, _yaw;
+    tf2::Matrix3x3(q).getRPY(_roll, _pitch, _yaw);
+    _theta = _yaw;
     RCLCPP_INFO(get_logger(), "Position(x: %f, y: %f, z: %f, theta: %f)", _odom_msg.pose.pose.position.x, _odom_msg.pose.pose.position.y, _odom_msg.pose.pose.position.z, _theta);
 }
